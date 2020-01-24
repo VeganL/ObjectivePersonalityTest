9de89d386fbc46dad72a34cecbfe23130d3c62ea
VeganL <veganlroy@gmail.com>
2020-01-24 12:14:12 -0500

    20200124 1213 Update: (ops.js) added new questions (WIP) and restructured/"finished" loadType()

--- ops.js
+++ ops.js
@@ -1,27 +1,27 @@
 var questions = {
     "firstQuestion": [
-        "You have trouble making other people understand, appreciate, or get on board with doing things or seeing things your way. Getting yourself on board with other's ideas may also be an issue.",
-        "Your life revolves around helping, giving advice, or solving problems for other people, and often, **** you either realize that not important to you or hit with imposter syndrome because can't help self ****.",
-        "You often encounter random things or ideas that threaten your long term goals or the integrity of pre-existing structures/organization that you utilize.",
-        "You have trouble devoting time to something for long enough to see your investments bear its fruits. You may have also even struggled to arrive at the decision to devote time because you wanted to keep your options open."
+        "You have trouble making other people understand, appreciate, or get on board with doing things or seeing things your way. Getting yourself on board with other's ideas or values may also be an issue.",
+        "Your life revolves around helping, giving advice, or solving problems for other people, but when it comes to helping yourself, you find that you're at a loss for what to do.",
+        "You often encounter random things or ideas that threaten your long term goals or the integrity of pre-existing structures/organizations that you utilize.",
+        "You have trouble devoting time to something for long enough to see your investments bear their fruits. You may have even struggled to arrive at the decision to devote time because you wanted to keep your options open."
         ],
-    "ipQuestion": ["Find the intake of new information to be more of a chore?",""], //FINISH QUESTIONS ON FLOW CHART AND PUT THEM HERE
-    "ipij": ["",""],
-    "ipep": ["",""],
+    "ipQuestion": ["Find the intake of new information to be more of a chore?","Spend a majority of your time learning new things?"],
+    "ipij": ["Have trouble conveying reasons to other people as to why you do things a certain way?","Have trouble handling or expressing your emotions or the emotions of other people?"], //FINISH QUESTIONS ON FLOW CHART AND PUT THEM HERE
+    "ipep": ["Have trouble conveying reasons to other people as to why you do things a certain way?","Have trouble handling or expressing your emotions or the emotions of other people?"],
     "IxFPS": ["",""],
     "IxTPS": ["",""],
     "IxFPC": ["",""],
     "IxTPC": ["",""],
 
-    "ejQuestion": ["",""],
-    "ejij": ["",""],
-    "ejep": ["",""],
+    "ejQuestion": ["Tend to interact with people in an improvisational, in-the-moment fashion?","Tend to interact with people in ways meant to organize some aspect of conduct?"],
+    "ejij": ["Have trouble expressing reasons that may threaten the status quo?","Find it takes a lot of effort to make decisions based on what you value when it can affect your group's goals?"],
+    "ejep": ["Have trouble expressing reasons that may threaten the status quo?","Find it takes a lot of effort to make decisions based on what you value when it can affect your group's goals?"],
     "ExFJB": ["",""],
     "ExTJB": ["",""],
     "ExFJP": ["",""],
     "ExTJP": ["",""],
 
-    "ijQuestion": ["",""],
+    "ijQuestion": ["Feel naturally obligated to share information and plans or manage the conduct of other people?","Have an inclination to continually revise plans and structure of personal life (whether mentally, or in a tangible manner) and have trouble sharing information to yourself?"],
     "ijip": ["",""],
     "ijej": ["",""],
     "ISxJS": ["",""],
@@ -188,102 +188,170 @@ function enableNext(opt) {
 }
 
 function loadType(type) {
-    switch (type) {
+    let type512;
+    let mbti;
+    let desc;
+    switch (type) { //TODO: Type descriptions
         case "1.1.1.1":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Ni-S (ISFP Jumper)</h2><p><img src="types/FiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'FiNi-S';
+            mbti = 'ISFP Jumper';
+            desc = '';
             break;
         case "1.1.1.2":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Si-S (INFP Jumper)</h2><p><img src="types/FiSi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'FiSi-S';
+            mbti = 'INFP Jumper';
+            desc = '';
             break;
         case "1.1.2.1":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Ti/Ni-S (ISTP Jumper)</h2><p><img src="types/TiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'TiNi-S';
+            mbti = 'ISTP Jumper';
+            desc = '';
             break;
         case "1.1.2.2":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Ti/Si-S (INTP Jumper)</h2><p><img src="types/TiSi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'TiSi-S';
+            mbti = 'INTP Jumper';
+            desc = '';
             break;
-        case "1.2.1.1": //FROM HERE ON IS UNFINISHED (INCOMPLETE)
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Ni-S (ISFP Jumper)</h2><p><img src="types/FiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+        case "1.2.1.1":
+            type512 = 'FiSe-C';
+            mbti = 'ISFP';
+            desc = '';
             break;
         case "1.2.1.2":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Ni-S (ISFP Jumper)</h2><p><img src="types/FiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'FiNe-C';
+            mbti = 'INFP';
+            desc = '';
             break;
         case "1.2.2.1":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Ni-S (ISFP Jumper)</h2><p><img src="types/FiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'TiSe-C';
+            mbti = 'ISTP';
+            desc = '';
             break;
         case "1.2.2.2":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Ni-S (ISFP Jumper)</h2><p><img src="types/FiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'TiNe-C';
+            mbti = 'INTP';
+            desc = '';
             break;
         case "2.1.1.1":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Ni-S (ISFP Jumper)</h2><p><img src="types/FiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'FeSi-B';
+            mbti = 'ESFJ';
+            desc = '';
             break;
         case "2.1.1.2":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Ni-S (ISFP Jumper)</h2><p><img src="types/FiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'FeNi-B';
+            mbti = 'ENFJ';
+            desc = '';
             break;
         case "2.1.2.1":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Ni-S (ISFP Jumper)</h2><p><img src="types/FiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'TeSi-B';
+            mbti = 'ESTJ';
+            desc = '';
             break;
         case "2.1.2.2":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Ni-S (ISFP Jumper)</h2><p><img src="types/FiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'TeNi-B';
+            mbti = 'ENTJ';
+            desc = '';
             break;
         case "2.2.1.1":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Ni-S (ISFP Jumper)</h2><p><img src="types/FiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'FeNe-P';
+            mbti = 'ESFJ Jumper';
+            desc = '';
             break;
         case "2.2.1.2":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Ni-S (ISFP Jumper)</h2><p><img src="types/FiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'FeSe-P';
+            mbti = 'ENFJ Jumper';
+            desc = '';
             break;
         case "2.2.2.1":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Ni-S (ISFP Jumper)</h2><p><img src="types/FiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'TeNe-P';
+            mbti = 'ESTJ Jumper';
+            desc = '';
             break;
         case "2.2.2.2":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Ni-S (ISFP Jumper)</h2><p><img src="types/FiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'TeSe-P';
+            mbti = 'ENTJ Jumper';
+            desc = '';
             break;
         case "3.1.1.1":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Ni-S (ISFP Jumper)</h2><p><img src="types/FiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'SiTi-S';
+            mbti = 'ISFJ Jumper';
+            desc = '';
             break;
         case "3.1.1.2":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Ni-S (ISFP Jumper)</h2><p><img src="types/FiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'SiFi-S';
+            mbti = 'ISTJ Jumper';
+            desc = '';
             break;
         case "3.1.2.1":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Ni-S (ISFP Jumper)</h2><p><img src="types/FiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'NiTi-S';
+            mbti = 'INFJ Jumper';
+            desc = '';
             break;
         case "3.1.2.2":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Ni-S (ISFP Jumper)</h2><p><img src="types/FiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'NiFi-S';
+            mbti = 'INTJ Jumper';
+            desc = '';
             break;
         case "3.2.1.1":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Ni-S (ISFP Jumper)</h2><p><img src="types/FiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'SiFe-B';
+            mbti = 'ISFJ';
+            desc = '';
             break;
         case "3.2.1.2":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Ni-S (ISFP Jumper)</h2><p><img src="types/FiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'SiTe-B';
+            mbti = 'ISTJ';
+            desc = '';
             break;
         case "3.2.2.1":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Ni-S (ISFP Jumper)</h2><p><img src="types/FiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'NiFe-B';
+            mbti = 'INFJ';
+            desc = '';
             break;
         case "3.2.2.2":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Ni-S (ISFP Jumper)</h2><p><img src="types/FiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'NiTe-B';
+            mbti = 'INTJ';
+            desc = '';
             break;
         case "4.1.1.1":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Ni-S (ISFP Jumper)</h2><p><img src="types/FiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'SeFi-C';
+            mbti = 'ESFP';
+            desc = '';
             break;
         case "4.1.1.2":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Ni-S (ISFP Jumper)</h2><p><img src="types/FiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'SeTi-C';
+            mbti = 'ESTP';
+            desc = '';
             break;
         case "4.1.2.1":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Ni-S (ISFP Jumper)</h2><p><img src="types/FiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'NeFi-C';
+            mbti = 'ENFP';
+            desc = '';
             break;
         case "4.1.2.2":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Ni-S (ISFP Jumper)</h2><p><img src="types/FiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'NeTi-C';
+            mbti = 'ENTP';
+            desc = '';
             break;
         case "4.2.1.1":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Ni-S (ISFP Jumper)</h2><p><img src="types/FiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'SeTe-P';
+            mbti = 'ESFP Jumper';
+            desc = '';
             break;
         case "4.2.1.2":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Ni-S (ISFP Jumper)</h2><p><img src="types/FiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'SeFe-P';
+            mbti = 'ESTP Jumper';
+            desc = '';
             break;
         case "4.2.2.1":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Ni-S (ISFP Jumper)</h2><p><img src="types/FiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'NeTe-P';
+            mbti = 'ENFP Jumper';
+            desc = '';
             break;
         case "4.2.2.2":
-            document.getElementById("test-content").innerHTML = '<h2>You have tested as Fi/Ni-S (ISFP Jumper)</h2><p><img src="types/FiNi-S.png"></p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
+            type512 = 'NeFe-P';
+            mbti = 'ENTP Jumper';
+            desc = '';
             break;
     }
+    document.getElementById("test-content").innerHTML = '<h2>You have tested as ' + type512.substr(0,2) + '/' + type512.substr(2,5) + ' (' + mbti + ')</h2><p><img src="types/' + type512 + '.png" alt="Type image for ' + type512.substr(0,2) + '/' + type512.substr(2,5) + '"></p><p>' + desc + '</p><p id="dashboard"><button class="dash" onclick="window.location.href = \'index.html\'">RETAKE TEST</button></p>';
 }
